583189bdcc0b343df68ed90a2fc4e8d87746960c
zhukata <zhukata@gmail.com>
2024-09-18 17:00:46 +0300

    fix filter.py

--- task_manager/tasks/filters.py
+++ task_manager/tasks/filters.py
@@ -3,11 +3,23 @@ from django import forms
 from django.utils.translation import gettext as _
 
 from task_manager.labels.models import Label
-from task_manager.tasks.models import Task
+from task_manager.statuses.models import Status
+from django.contrib.auth import get_user_model
 
 
 class TaskFilter(django_filters.FilterSet):
-    labels = django_filters.ModelChoiceFilter(queryset=Label.objects.all())
+    status = django_filters.ModelChoiceFilter(
+        queryset=Status.objects.all(),
+        label=_('Status')
+    )
+    executor = django_filters.ModelChoiceFilter(
+        queryset=get_user_model().objects.all(),
+        label=_('Executor')
+    )
+    labels = django_filters.ModelChoiceFilter(
+        queryset=Label.objects.all(),
+        label=_('Labels')
+    )
     only_author = django_filters.BooleanFilter(
         field_name='author',
         label=_('Only your tasks'),
@@ -15,10 +27,10 @@ class TaskFilter(django_filters.FilterSet):
         widget=forms.CheckboxInput
     )
 
-    class Meta:
-        model = Task
-        fields = ['status', 'executor', 'labels', 'only_author']
-        labels = {'status': _('Status'), 'executor': _('Executor'), 'labels': _('Label'), }
+    # class Meta:
+    #     model = Task
+    #     fields = ['status', 'executor', 'labels', 'only_author']
+    #     labels = {'status': _('Status'), 'executor': _('Executor'), }
 
     def author_filter(self, queryset, name, value):
         if value:
