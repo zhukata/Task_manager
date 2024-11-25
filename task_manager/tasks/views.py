a1131186934f3801417f6e1c32c3a42dc0d69acf
zhukata <zhukata@gmail.com>
2024-11-25 20:35:22 +0300

    add jwt auth

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -63,3 +63,4 @@ class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     permission_classes = (IsAuthenticated,)
+    # authentication_classes = (Token)
--- task_manager/urls.py
+++ task_manager/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
 
 from task_manager import views
 from task_manager.tasks.views import TaskViewSet
@@ -36,5 +37,7 @@ urlpatterns = [
     path('logout/', views.UserLogoutView.as_view(), name='user_logout'),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('api/token/', obtain_auth_token),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
